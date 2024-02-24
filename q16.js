da431ae3da3aefadf47276979ec24f2fef50cc50
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 03:47:18 +0500

    Add files via upload

---
+++ q16.js
@@ -0,0 +1,34 @@
+"use strict";
+/* Ques # 16:
+More Guest: You just found a bigger dinner table,so now more space is available. Think of three more guests to invite a dinner.
+
+ . Start with your program from exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+
+ . Add one new guest to the beginning of your array.
+
+ . Add one new guest to the middle of your array.
+
+ . Use append()to add one new guest to the end of your list.
+
+ . Print a new set of invitation messages, one for each person in your list.*/
+let guestArr = ["Sana", "Uzma", "Mariyam", "Hassan"];
+let canNotAttend = "Sana";
+let newGuest = "Aqsa";
+guestArr[guestArr.indexOf(canNotAttend)] = newGuest;
+//console.log(guestArr);
+/*
+guestArr.map((item) => {
+  console.log(`Dear ${item}, I found a bigger Dinner table.`)
+})*/
+let guestBegin = "Amber";
+guestArr.unshift(guestBegin);
+//console.log(guestArr);
+let middleGuest = "Rida";
+let middleIndex = guestArr.length / 2;
+guestArr.splice(middleIndex, 0, middleGuest);
+//console.log(guestArr);
+guestArr.push("Fatima");
+console.log(guestArr);
+guestArr.map((item) => {
+    console.log(`Dear ${item}, You are cordinally invited to a Dinner.`);
+});
